230990cc1022eefcb29e0b729cfbf4ccaeda7caa
Ahmed Maawy <ultimateprogramer@gmail.com>
2013-02-19 21:40:33 +0300

    Enhancements to the CMSInit functionality

--- fuel/app/classes/CMSInit.php
+++ fuel/app/classes/CMSInit.php
@@ -16,9 +16,13 @@ class CMSInit {
      * @return void
      */
 
+    private static $installed_extensions = array();
+    private static $installed_themes = array();
+
     public static function load_extension($extension_folder)
     {
         Module::load($extension_folder, EXTPATH.$extension_folder.DIRECTORY_SEPARATOR);
+        self::$installed_extensions[] = $extension_folder;
     }
 
     /**
@@ -31,6 +35,7 @@ class CMSInit {
     public static function load_theme($theme_folder)
     {
         Module::load($theme_folder, THEMEPATH.$theme_folder.DIRECTORY_SEPARATOR);
+        self::$installed_themes[] = $theme_folder;
     }
 
     /**
@@ -52,5 +57,43 @@ class CMSInit {
         foreach($installed_themes as $installed_theme) {
             self::load_theme($installed_theme->theme_folder);
         }
+    }
+
+    /**
+     * Verifies if an extension is installed
+     *
+     * @static
+     * @param $extension_name
+     * @return bool
+     */
+
+    public static function is_extension_installed($extension_name)
+    {
+        foreach(self::$installed_extensions as $installed_extension)
+        {
+            if(strtolower($installed_extension) == strtolower($extension_name))
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Verifies if the theme is installed
+     *
+     * @static
+     * @param $theme_name
+     * @return bool
+     */
+
+    public static function is_theme_installed($theme_name)
+    {
+        foreach(self::$installed_themes as $installed_theme)
+        {
+            if(strtolower($installed_theme) == strtolower($theme_name))
+                return true;
+        }
+
+        return false;
     }
 }
